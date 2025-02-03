e34f8d0a2683d3c91c7a9b2614174d7cc86e3da5
David Barroso <dbarrosop@dravetech.com>
2025-02-03 15:08:11 +0100

    asd

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,7 +4,7 @@
   nhost-cli = final.callPackage ./nhost-cli.nix { inherit final; };
 
   pnpm_10 = final.callPackage "${final.path}/pkgs/development/tools/pnpm/generic.nix" {
-    version = "10.1.0";
-    hash = "sha256-PuU+kUAR7H8abjqwxYuaAkoFK/4YKVsjtoVn1qal680=";
+    version = "10.0.0";
+    hash = "sha256-Q6v25yD7e8U8WRsIYmBcfTI9Cp0t0zvKwHsGLhPPSUg=";
   };
 })
