9a75d70cce015aba4daaa35a48450321586b8211
David B.M. <correodelnino@gmail.com>
2025-02-13 02:37:28 +0100

    feat: add graphite ui iframe for localhost

--- dashboard/src/pages/orgs/[orgSlug]/projects/[appSubdomain]/ai/auto-embeddings/index.tsx
+++ dashboard/src/pages/orgs/[orgSlug]/projects/[appSubdomain]/ai/auto-embeddings/index.tsx
@@ -1,91 +1,80 @@
 /* eslint-disable import/extensions */
-import { useDialog } from '@/components/common/DialogProvider';
-import { Pagination } from '@/components/common/Pagination';
 import { UpgradeToProBanner } from '@/components/common/UpgradeToProBanner';
 import { Container } from '@/components/layout/Container';
-import { RetryableErrorBoundary } from '@/components/presentational/RetryableErrorBoundary';
+import { ActivityIndicator } from '@/components/ui/v2/ActivityIndicator';
 import { Alert } from '@/components/ui/v2/Alert';
 import { Box } from '@/components/ui/v2/Box';
-import { Button } from '@/components/ui/v2/Button';
-import { EmbeddingsIcon } from '@/components/ui/v2/icons/EmbeddingsIcon';
-import { PlusIcon } from '@/components/ui/v2/icons/PlusIcon';
 import { Link } from '@/components/ui/v2/Link';
 import { Text } from '@/components/ui/v2/Text';
+
+import { type GetAssistantsQuery } from '@/utils/__generated__/graphite.graphql';
+import { useEffect, useRef, type ReactElement } from 'react';
+
+import { RetryableErrorBoundary } from '@/components/presentational/RetryableErrorBoundary';
 import { AISidebar } from '@/features/orgs/layout/AISidebar';
 import { ProjectLayout } from '@/features/orgs/layout/ProjectLayout';
-import { AutoEmbeddingsForm } from '@/features/orgs/projects/ai/AutoEmbeddingsForm';
-import { AutoEmbeddingsList } from '@/features/orgs/projects/ai/AutoEmbeddingsList';
 import { useIsGraphiteEnabled } from '@/features/orgs/projects/common/hooks/useIsGraphiteEnabled';
 import { useIsPlatform } from '@/features/orgs/projects/common/hooks/useIsPlatform';
-import { useAdminApolloClient } from '@/features/orgs/projects/hooks/useAdminApolloClient';
 import { useCurrentOrg } from '@/features/orgs/projects/hooks/useCurrentOrg';
 import { useProject } from '@/features/orgs/projects/hooks/useProject';
-import {
-  useGetGraphiteAutoEmbeddingsConfigurationsQuery,
-  type GetGraphiteAutoEmbeddingsConfigurationsQuery,
-} from '@/utils/__generated__/graphite.graphql';
-import { useRouter } from 'next/router';
-import { useEffect, useMemo, useRef, useState, type ReactElement } from 'react';
+import { useNhostClient } from '@nhost/nextjs';
 
-export type AutoEmbeddingsConfiguration = Omit<
-  GetGraphiteAutoEmbeddingsConfigurationsQuery['graphiteAutoEmbeddingsConfigurations'][0],
+export type Assistant = Omit<
+  GetAssistantsQuery['graphite']['assistants'][0],
   '__typename'
 >;
 
 export default function AutoEmbeddingsPage() {
-  const limit = useRef(25);
-  const router = useRouter();
-
-  const { openDrawer } = useDialog();
   const isPlatform = useIsPlatform();
+  const { isGraphiteEnabled, loading: loadingGraphite } =
+    useIsGraphiteEnabled();
 
-  const { org } = useCurrentOrg();
-  const { project } = useProject();
+  const { org, loading: loadingOrg } = useCurrentOrg();
+  const { project, loading: loadingProject } = useProject();
 
-  const { adminClient } = useAdminApolloClient();
-  const { isGraphiteEnabled } = useIsGraphiteEnabled();
-
-  const [currentPage, setCurrentPage] = useState(
-    parseInt(router.query.page as string, 10) || 1,
-  );
-  const [nrOfPages, setNrOfPages] = useState(0);
-  const offset = useMemo(() => currentPage - 1, [currentPage]);
-
-  const { data, loading, refetch } =
-    useGetGraphiteAutoEmbeddingsConfigurationsQuery({
-      client: adminClient,
-      variables: {
-        limit: limit.current,
-        offset,
-      },
-    });
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const nhost = useNhostClient();
 
   useEffect(() => {
-    if (loading) {
+    const session = nhost?.auth?.getSession();
+    if (!session) {
       return;
     }
-
-    const autoEmbeddingsCount =
-      data?.graphiteAutoEmbeddingsConfigurationAggregate?.aggregate.count ?? 0;
-
-    setNrOfPages(Math.ceil(autoEmbeddingsCount / limit.current));
-  }, [data, loading]);
-
-  const autoEmbeddingsConfigurations = useMemo(
-    () => data?.graphiteAutoEmbeddingsConfigurations || [],
-    [data],
-  );
-
-  const openCreateAutoEmbeddingsConfiguration = () => {
-    openDrawer({
-      title: (
-        <Box className="flex flex-row items-center space-x-2">
-          <Text>Create new Auto-Embeddings configuration</Text>
-        </Box>
-      ),
-      component: <AutoEmbeddingsForm onSubmit={refetch} />,
+    window.addEventListener('message', async (event) => {
+      // if (event.origin !== 'https://your-allowed-origin.com') return;
+      const { type } = event.data;
+      if (type === 'GRAPHITE_READY') {
+        iframeRef.current?.contentWindow?.postMessage(
+          { type: 'NHOST_SESSION', payload: session },
+          '*',
+        );
+      }
     });
-  };
+  }, [nhost?.auth]);
+
+  // useEffect(() => {
+  //   const session = nhost.auth.getSession();
+  //   if (session) {
+  //     console.log('session', session);
+  //     iframeRef.current?.contentWindow?.postMessage(
+  //       { type: 'NHOST_SESSION_CLIENT', payload: nhost },
+  //       '*',
+  //     );
+  //     console.log('postMessage', session.accessToken);
+  //   }
+  // }, [nhost]);
+
+  if (loadingOrg || loadingProject || loadingGraphite) {
+    return (
+      <Box className="flex h-full w-full items-center justify-center">
+        <ActivityIndicator
+          delay={1000}
+          label="Loading Assistants..."
+          className="justify-center"
+        />
+      </Box>
+    );
+  }
 
   if (isPlatform && org?.plan?.isFree) {
     return (
@@ -94,7 +83,7 @@ export default function AutoEmbeddingsPage() {
         rootClassName="bg-transparent"
       >
         <UpgradeToProBanner
-          title="To unlock Auto-Embeddings, transfer this project to a Pro or Team organization."
+          title="To unlock Nhost Assistants, transfer this project to a Pro or Team organization."
           description=""
         />
       </Container>
@@ -102,7 +91,7 @@ export default function AutoEmbeddingsPage() {
   }
 
   if (
-    (isPlatform && !org?.plan?.isFree && !project?.config?.ai) ||
+    (isPlatform && !org?.plan?.isFree && !project.config?.ai) ||
     !isGraphiteEnabled
   ) {
     return (
@@ -129,93 +118,21 @@ export default function AutoEmbeddingsPage() {
     );
   }
 
-  if (autoEmbeddingsConfigurations.length === 0 && !loading) {
-    return (
-      <Box
-        className="w-full p-6"
-        sx={{ backgroundColor: 'background.default' }}
-      >
-        <Box className="flex flex-col items-center justify-center space-y-5 rounded-lg border px-48 py-12 shadow-sm">
-          <EmbeddingsIcon className="h-10 w-10" />
-          <div className="flex flex-col space-y-1">
-            <Text className="text-center font-medium" variant="h3">
-              No Auto-Embeddings are configured
-            </Text>
-            <Text variant="subtitle1" className="text-center">
-              All your configurations will be listed here.
-            </Text>
-          </div>
-          <div className="flex flex-row place-content-between rounded-lg">
-            <Button
-              variant="contained"
-              color="primary"
-              className="w-full"
-              onClick={openCreateAutoEmbeddingsConfiguration}
-              startIcon={<PlusIcon className="h-4 w-4" />}
-            >
-              Add a new Auto-Embeddings Configuration
-            </Button>
-          </div>
-        </Box>
-      </Box>
-    );
-  }
-
   return (
-    <Box className="flex w-full flex-col overflow-hidden">
-      <Box className="flex flex-row place-content-end border-b-1 p-4">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={openCreateAutoEmbeddingsConfiguration}
-          startIcon={<PlusIcon className="h-4 w-4" />}
-        >
-          New
-        </Button>
-      </Box>
-      <div>
-        <AutoEmbeddingsList
-          autoEmbeddingsConfigurations={autoEmbeddingsConfigurations}
-          onDelete={() => refetch()}
-          onCreateOrUpdate={() => refetch()}
-        />
-
-        <Pagination
-          className="px-2 py-4"
-          totalNrOfPages={nrOfPages}
-          currentPageNumber={currentPage}
-          totalNrOfElements={
-            data?.graphiteAutoEmbeddingsConfigurationAggregate?.aggregate
-              ?.count ?? 0
-          }
-          itemsLabel="Auto-Embeddings Configurations"
-          elementsPerPage={limit.current}
-          onPrevPageClick={async () => {
-            setCurrentPage((page) => page - 1);
-            if (currentPage - 1 !== 1) {
-              await router.push({
-                pathname: router.pathname,
-                query: { ...router.query, page: currentPage - 1 },
-              });
-            }
-          }}
-          onNextPageClick={async () => {
-            setCurrentPage((page) => page + 1);
-            await router.push({
-              pathname: router.pathname,
-              query: { ...router.query, page: currentPage + 1 },
-            });
-          }}
-          onPageChange={async (page) => {
-            setCurrentPage(page);
-            await router.push({
-              pathname: router.pathname,
-              query: { ...router.query, page },
-            });
-          }}
-        />
-      </div>
-    </Box>
+    <Container
+      className="grid max-w-5xl grid-flow-row gap-y-6 bg-transparent"
+      rootClassName="bg-transparent"
+    >
+      <iframe
+        ref={iframeRef}
+        // src="https://graphite-ui-iframe.vercel.app/auto-embeddings"
+        src={`http://localhost:3000/orgs/${org?.slug}/projects/${project?.subdomain}/auto-embeddings`}
+        title="Graphite UI"
+        width="100%"
+        height="600px"
+        style={{ border: 'none' }}
+      />
+    </Container>
   );
 }
 
